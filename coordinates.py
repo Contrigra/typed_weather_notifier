ddeb1e55fdf9c4e340b4008291fca8868843da9c
Contrigra <gaponov60b@gmail.com>
2022-06-21 16:16:57 +0300

    weather_formatter.py: added a dummy function the structure

--- coordinates.py
+++ coordinates.py
@@ -8,6 +8,8 @@ class Coordinates(NamedTuple):
     longitude: float
 
 
+# TODO find and obtain my GPS data
+
 def get_gps_coordinates() -> Coordinates:
     """Returns current coordinates using GPS app"""
     return Coordinates(longitude=10, latitude=20)
---
+++ weather_formatter.py
@@ -0,0 +1,9 @@
+from weather_api_service import Weather
+
+
+# TODO
+def format_weather(weather: Weather) -> str:
+    """Formats weather data in string"""
+
+    pass
+
