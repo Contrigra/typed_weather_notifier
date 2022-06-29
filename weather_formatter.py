4fd74eeeae62a26444aa71791f5175581ebdf143
Contrigra <gaponov60b@gmail.com>
2022-06-29 11:22:31 +0300

    weather_formatter.py.py: done
    added __name__ == "__main__" block

--- weather_formatter.py
+++ weather_formatter.py
@@ -1,8 +1,21 @@
 from weather_api_service import Weather
 
 
-# TODO
 def format_weather(weather: Weather) -> str:
     """Formats weather data in string"""
-    pass
+    return (f"{weather.city}, температура {weather.temperature}°C, "
+            f"{weather.weather_type}\n"
+            f"Восход: {weather.sunrise.strftime('%H:%M')}\n"
+            f"Закат: {weather.sunset.strftime('%H:%M')}\n")
 
+
+if __name__ == "__main__":
+    from datetime import datetime
+    from weather_api_service import WeatherType
+    print(format_weather(Weather(
+        temperature=15,
+        weather_type=WeatherType.DRIZZLE,
+        sunrise=datetime.fromisoformat("2022-06-25 03:30:00"),
+        sunset=datetime.fromisoformat("2022-06-25 21:00:00"),
+        city="Irkutsk"
+    )))
